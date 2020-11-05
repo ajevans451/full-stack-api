2be2c4ea27bc452abadcc9c74453807cb5940aa9
ajevans <ajecoder@gmail.com>
2020-11-05 13:36:31 -0500

    add char schema

--- models/character.js
+++ models/character.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose')
 const characterSchema = new mongoose.Schema({
-
+  name: {
+    type: String
+  },
+  race: {
+    type: String
+  },
+  class: {
+    type: String
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, {
+  timestamps: true
 })
 module.exports = mongoose.model('Character', characterSchema)
