03e91f6e96f1f6f71f218d633173098dc24107f2
ajevans <ajecoder@gmail.com>
2020-11-05 13:25:24 -0500

    finish router

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
+const passport = require('passport')
+const requireToken = passport.authenticate('bearer', { session: false })
 const User = require('../models/user')
 const router = express.Router()
 router.post('/sign-up', (req, res, next) => {
@@ -38,7 +40,7 @@ router.post('/sign-in', (req, res, next) => {
       return bcrypt.compare(password, user.hashedPassword)
     })
     .then(correctPW => {
-      if(correctPW) {
+      if (correctPW) {
         const token = crypto.randomBytes(16).toString('hex')
         user.token = token
         return user.save()
@@ -51,3 +53,33 @@ router.post('/sign-in', (req, res, next) => {
     })
     .catch(next)
 })
+router.patch('/change-password', requireToken, (req, res, next) => {
+  let user
+  User.findById(req.user.id)
+    .then(foundUser => {
+      if (!foundUser) {
+        throw new Error('User not found')
+      }
+      user = foundUser
+      return bcrypt.compare(req.body.passwords.old, user.hashedPassword)
+    })
+    .then(correctPW => {
+      if (!correctPW || !req.body.passwords.new) {
+        throw new Error('Omitted or invalid params')
+      }
+      return bcrypt.hash(req.body.passwords.new, 12)
+    })
+    .then(hash => {
+      user.hashedPassword = hash
+      return user.save()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+router.delete('/sign-out', requireToken, (req, res, next) => {
+  req.user.token = crypto.randomBytes(16)
+  req.user.save()
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+module.exports = router
