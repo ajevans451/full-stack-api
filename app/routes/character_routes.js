715693d827b7059468e3c153163b9518a1a91ef5
ajevans <ajecoder@gmail.com>
2020-11-06 12:41:18 -0500

    add char router file

---
+++ app/routes/character_routes.js
@@ -0,0 +1,11 @@
+// Express docs: http://expressjs.com/en/api.html
+const express = require('express')
+// Passport docs: http://www.passportjs.org/docs/
+const passport = require('passport')
+const Character = require('../models/character')
+const customErrors = require('../../lib/custom_errors')
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const removeBlanks = require('../../lib/remove_blank_fields')
+const requireToken = passport.authenticate('bearer', { session: false })
+const router = express.Router()
